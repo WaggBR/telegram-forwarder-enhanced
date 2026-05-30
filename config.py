29600c63fe8460b1ba7a45e2c3230e5556a03172
Wagg13 <57603689+WaggBR@users.noreply.github.com>
2026-05-30 00:46:56 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
-    API_ID = "12345678"                  # Seu API ID
-    API_HASH = "seu_api_hash"            # Seu API Hash
-    PHONE_NUMBER = "+5511999999999"      # Seu número com código do país
-    NAME = "telegram-forwarder"          # Nome escolhido por ti
-    SOURCE_CHAT_ID = -1001234567890      # ID do canal/grupo origem
-    DESTINATION_CHAT_ID = -1009876543210 # ID do canal/grupo destino
+    API_ID = "12345678"                  # Seu API ID/ Your API ID
+    API_HASH = "seu_api_hash"            # Seu API Hash/ Your API Hash
+    PHONE_NUMBER = "+5511999999999"      # Seu número com código do país/ Your number with country code
+    NAME = "telegram-forwarder"          # Nome escolhido por ti/ Chosen name
+    SOURCE_CHAT_ID = -1001234567890      # ID do canal/grupo origem / Source channel/group ID
+    DESTINATION_CHAT_ID = -1009876543210 # ID do canal/grupo destino / Destination channel/group ID
